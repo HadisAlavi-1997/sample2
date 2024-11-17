f5c8450719609bb430252d6022b4260888c64c08
HadisAlavi-1997 <msalavi.1997@gmail.com>
2024-11-17 13:07:03 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,43 +24,43 @@ class MyApp extends StatelessWidget {
                   textStyle: MaterialStateProperty.all(const TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.w500,
-                    fontFamily:'Avenir',
+                    fontFamily: 'Avenir',
                   )))),
           textTheme: const TextTheme(
             labelMedium: TextStyle(
-              fontFamily: 'Avenir',
-               fontSize: 14,
-              fontWeight: FontWeight.w700,
-              color: Color(0xff376AED)
-            ),
-              headlineSmall:
-                  TextStyle(fontFamily: 'Avenir',
-                      color: primaryTextColor,
-                      fontSize:18
-                  ),
-
-              headlineMedium: TextStyle(
-                  fontFamily: 'Avenir',
-                  fontWeight: FontWeight.bold,
-                  color: primaryTextColor,
-                  fontSize: 20),
-
-              titleLarge: TextStyle(
-                  fontFamily: 'Avenir', color: secondTextColor, fontSize: 20),
-              titleSmall: TextStyle(
                 fontFamily: 'Avenir',
- 
-              ),
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+                color: Color(0xff376AED)
+            ),
+            headlineSmall:
+            TextStyle(fontFamily: 'Avenir',
+                color: primaryTextColor,
+                fontSize: 18
+            ),
+
+            headlineMedium: TextStyle(
+                fontFamily: 'Avenir',
+                fontWeight: FontWeight.bold,
+                color: primaryTextColor,
+                fontSize: 20),
+
+            titleLarge: TextStyle(
+                fontFamily: 'Avenir', color: secondTextColor, fontSize: 20),
+            titleSmall: TextStyle(
+              fontFamily: 'Avenir',
+
+            ),
             bodySmall: TextStyle(
                 fontFamily: FontFamily.avenir,
                 fontWeight: FontWeight.w500,
                 color: Color(0xff7B8BB2),
-                 
+
                 fontSize: 8),
           )),
 
       home: const SplashScreen(),
- 
-              );
 
+    );
   }
+}
