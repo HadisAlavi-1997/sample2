6a9d476a290e6fd4c2698156ccaaa53032e1b821
HadisAlavi-1997 <msalavi.1997@gmail.com>
2024-11-17 10:15:11 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Avenir', color: secondTextColor, fontSize: 20),
               titleSmall: TextStyle(
                 fontFamily: 'Avenir',
-<<<<<<< HEAD
+ 
               ),
             bodySmall: TextStyle(
                 fontFamily: FontFamily.avenir,
@@ -60,11 +60,11 @@ class MyApp extends StatelessWidget {
           )),
 
       home: const SplashScreen(),
-=======
+ 
               ))),
       home: const Home(),
-      //milad
->>>>>>> 3698dcb429b3bc133d48382f20d80c85a3ddb146
+      
+ 
     );
   }
 }
